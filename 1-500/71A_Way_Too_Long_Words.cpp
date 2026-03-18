c21686c9c90c9dbdbbb38a589e16adbaba6a2840
Jaykape <natthajakntk@gmail.com>
2026-03-18 23:21:30 +0700

    71A

---
+++ 1-500/71A_Way_Too_Long_Words.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        if (s.size() < 11)
+            cout << s << endl;
+        else
+            cout << s[0] << s.size() - 2 << s[s.size() - 1] << endl;
+    }
+    return 0;
+}
