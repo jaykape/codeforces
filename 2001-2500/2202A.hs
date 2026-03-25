4385e949f7c0a82da2d328d267832712e22ac5fe
Jaykape <natthajakntk@gmail.com>
2026-03-25 22:34:24 +0700

    2202A

--- 2001-2500/2202A.hs
+++ 2001-2500/2202A.hs
@@ -1,8 +1,9 @@
+import Control.Monad (replicateM_)
 main = do
   t <- readLn
-  sequence_ [solve | _ <- [1..t]]
+  replicateM_ t runCase
 
-solve = do
+runCase = do
   [x,y] <- fmap (map read . words) getLine
   if (x `div` 2 >= y && y >= (-x `div` 4)) && ((x + y) `mod` 3 == 0)
     then putStrLn "YES"
