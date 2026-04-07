30da9d5f323ef27d08364e10e75533045e8e1d9e
Jaykape <natthajakntk@gmail.com>
2026-04-07 19:53:55 +0700

    1740A

---
+++ 1501-2000/1740A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        if (n == 2)
+            cout << 2 << endl;
+        else
+            cout << 3 << endl;
+    }
+    return 0;
+}
