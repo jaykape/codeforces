8f4e0105f01b4cf895d4af7ee41e592eb962e189
Jaykape <natthajakntk@gmail.com>
2026-04-08 02:05:19 +0700

    630J

---
+++ 501-1000/630J.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: i64 = input.trim().parse().unwrap();
+
+    let result = n / 2520;
+    println!("{}", result);
+}
