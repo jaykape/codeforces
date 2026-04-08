8527b4aa62b3204a4de26c0b194fc4899597cfa6
Jaykape <natthajakntk@gmail.com>
2026-04-08 17:10:36 +0700

    fix trim node template

--- templates/node.js
+++ templates/node.js
@@ -2,7 +2,7 @@
 let input = '';
 process.stdin.on('data', chunk => input += chunk);
 process.stdin.on('end', () => {
-    let lines = input.trim.split('\n');
+    let lines = input.trim().split('\n');
 
 
 
