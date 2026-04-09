1a87afa589a786633d4aa18ffca606dc2861d616
Jaykape <natthajakntk@gmail.com>
2026-04-09 15:51:18 +0700

    2034A

---
+++ 2001-2500/2034A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << lcm(a, b) << endl;
+    }
+    return 0;
+}
